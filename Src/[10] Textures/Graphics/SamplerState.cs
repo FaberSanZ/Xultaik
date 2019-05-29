3ec41f54400ddc83c08eed7761da2d15997990eb
Faber Leonardo Sanchez Hidalgo <faber.reach@gmail.com>
2019-05-28 22:33:57 -0500

    Update Textures

---
+++ Src/[10] Textures/Graphics/SamplerState.cs
@@ -0,0 +1,51 @@
+﻿using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphics
+{
+    public class SamplerState
+    {
+        public SharpDX.Direct3D11.SamplerState Sampler { get; set; }
+
+        public GraphicsDevice GraphicsDevice { get; }
+
+
+        public TextureAddressMode AddressU { get; }
+        public TextureAddressMode AddressV { get; }
+        public TextureAddressMode AddressW { get; }
+
+        public Filter Filter { get; }
+
+        public SamplerState(GraphicsDevice device, TextureAddressMode addressU, TextureAddressMode addressV, TextureAddressMode addressW, Filter filter)
+        {
+            GraphicsDevice = device;
+
+            //set address mode 
+            AddressU = addressU;
+            AddressV = addressV;
+            AddressW = addressW;
+
+            Filter = filter;
+
+
+            SamplerStateDescription SamplerDesc = new SamplerStateDescription();
+            SamplerDesc.Filter = Filter.MinMagMipLinear;
+            SamplerDesc.AddressU = AddressU;
+            SamplerDesc.AddressV = AddressV;
+            SamplerDesc.AddressW = AddressW;
+            SamplerDesc.MipLodBias = 0;
+            SamplerDesc.MaximumAnisotropy = 1;
+            SamplerDesc.ComparisonFunction = Comparison.Always;
+            SamplerDesc.BorderColor = new  SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 0);  // Black Border.
+            SamplerDesc.MinimumLod = 0;
+            SamplerDesc.MaximumLod = float.MaxValue;
+
+            Sampler = new SharpDX.Direct3D11.SamplerState(GraphicsDevice.NativeDevice, SamplerDesc);
+        }
+
+    }
+}
--- Src/[10] Textures/Shaders/VertexShader.hlsl
+++ Src/[10] Textures/Shaders/VertexShader.hlsl
@@ -1,38 +1,38 @@
 ﻿cbuffer MatrixBuffer
 {
-    matrix W;
-    matrix V;
+	matrix W;
+	matrix V;
     matrix P;
 };
 
 
 struct VertexInputType
 {
-    float4 Position : POSITION;
-    float2 Tex : TEXCOORD0;
+	float4 Position : POSITION;
+	float2 Tex : TEXCOORD0;
 };
 
 struct PixelInputType
 {
-    float4 Position : SV_POSITION;
-    float2 Tex : TEXCOORD0;
+	float4 Position : SV_POSITION;
+	float2 Tex : TEXCOORD0;
 };
 
 
 PixelInputType VS(VertexInputType input)
 {
-    PixelInputType output;
+	PixelInputType output;
 
 	// Change the position vector to be 4 units for proper matrix calculations.
-    input.Position.w = 1.0f;
+	input.Position.w = 1.0f;
 
 	// Calculate the position of the vertex against the world, view, and projection matrices.
-    output.Position = mul(input.Position, W);
-    output.Position = mul(output.Position, V);
+	output.Position = mul(input.Position, W);
+	output.Position = mul(output.Position, V);
     output.Position = mul(output.Position, P);
 
 	// Store the texture coordinates for the pixel shader to use.
-    output.Tex = input.Tex;
+	output.Tex = input.Tex;
 
-    return output;
+	return output;
 }
