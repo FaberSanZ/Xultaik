9bcd062fead6ed70a85c40c947ef69f8a38b8365
Faber Leonardo Sanchez Hidalgo <faber.reach@gmail.com>
2019-10-16 13:50:13 -0500

    Create BufferFlags

---
+++ Src/Zeckoxe.Graphics/BufferFlags.cs
@@ -0,0 +1,36 @@
+﻿// Copyright(c) 2019-2020 Faber Leonardo.All Rights Reserved.
+
+/*=============================================================================
+	BufferFlags.cs
+=============================================================================*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zeckoxe.Graphics
+{
+    [Flags]
+    public enum BufferFlags
+    {
+
+        None = 0,
+
+
+        ConstantBuffer = 1,
+
+
+        IndexBuffer = 2,
+
+
+        VertexBuffer = 4,
+
+
+        RenderTarget = 8,
+
+
+        ShaderResource = 16,
+
+    }
+}
