0f4186c5affd06eef6246e82f7b4d5f98f16e6e5
Faber Leonardo Sanchez Hidalgo <faber.reach@gmail.com>
2019-10-16 13:45:14 -0500

    Create CullMode

---
+++ Src/Zeckoxe.Graphics/CullMode.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2019-2020 Faber Leonardo. All Rights Reserved.
+
+/*=============================================================================
+	CullMode.cs
+=============================================================================*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zeckoxe.Graphics
+{
+    public enum CullMode : int
+    {
+        None = unchecked(0),
+
+        Front = unchecked(1),
+
+        Back = unchecked(2),
+    }
+}
