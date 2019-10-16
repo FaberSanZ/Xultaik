e2b3a4eff30d26d74f366eb104d24d3a550d935e
Faber Leonardo Sanchez Hidalgo <faber.reach@gmail.com>
2019-10-16 13:51:15 -0500

    Create BufferDescription

---
+++ Src/Zeckoxe.Graphics/BufferDescription.cs
@@ -0,0 +1,38 @@
+﻿
+// Copyright (c) 2019-2020 Faber Leonardo. All Rights Reserved.
+
+/*=============================================================================
+	BufferDescription.cs
+=============================================================================*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zeckoxe.Graphics
+{
+    public struct BufferDescription
+    {
+
+        public int SizeInBytes;
+
+
+        public int StructureByteStride;
+
+
+        public BufferFlags BufferFlags;
+
+
+
+
+        public BufferDescription(int sizeInBytes, BufferFlags bufferFlags, int structureByteStride = 0)
+        {
+            SizeInBytes = sizeInBytes;
+
+            BufferFlags = bufferFlags;
+
+            StructureByteStride = structureByteStride;
+        }
+
+    }
+}
