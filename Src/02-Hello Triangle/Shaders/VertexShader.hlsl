c961e3dda4da603cb9600b074f0a00fe0f4bf2a0
Faber Sanchez <faber.reach@gmail.com>
2019-12-07 21:06:01 -0500

    Update

---
+++ Src/02-Hello Triangle/Shaders/VertexShader.hlsl
@@ -0,0 +1,20 @@
+﻿struct VS_INPUT
+{
+	float3 pos : POSITION;
+	float4 color : COLOR;
+};
+
+struct VS_OUTPUT
+{
+	float4 pos : SV_POSITION;
+	float4 color : COLOR;
+};
+
+VS_OUTPUT main(VS_INPUT input)
+{
+	VS_OUTPUT output;
+	output.pos = float4(input.pos, 1.0f);
+	output.color = input.color;
+
+	return output;
+}
