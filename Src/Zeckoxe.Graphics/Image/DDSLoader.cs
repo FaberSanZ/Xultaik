39b656016cd6b0deaa03a60564d9d82e5a19eeac
Faber Sanchez <faber.reach@gmail.com>
2019-12-10 15:35:45 -0500

    Implement DDSLoader.DDSCaps2

--- Src/Zeckoxe.Graphics/Image/DDSLoader.cs
+++ Src/Zeckoxe.Graphics/Image/DDSLoader.cs
@@ -22,5 +22,28 @@ namespace Zeckoxe.Image
 
             Texture = 0x1000,
         }
+
+
+        [Flags]
+        enum DDSCaps2
+        {
+            Cubemap = 0x200,
+
+            CubemapPositiveX = 0x400,
+ 
+            CubemapNegativeX = 0x800,
+ 
+            CubemapPositiveY = 0x1000,
+
+            CubemapNegativeY = 0x2000,
+
+            CubemapPositiveZ = 0x4000,
+
+            CubemapNegativeZ = 0x8000,
+
+            Volume = 0x200000,
+
+            AllFaces = 0x200 | CubemapPositiveX | CubemapNegativeX | CubemapPositiveY | CubemapNegativeY | CubemapPositiveZ | CubemapNegativeZ,
+        }
     }
 }
