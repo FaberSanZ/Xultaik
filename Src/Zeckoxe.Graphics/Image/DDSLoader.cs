6e8b392e43c5d6e2ef9e2d78c0b4df6077ba4e7b
Faber Sanchez <faber.reach@gmail.com>
2019-12-10 16:00:12 -0500

    Implement DDSLoader.CubemapFlags

--- Src/Zeckoxe.Graphics/Image/DDSLoader.cs
+++ Src/Zeckoxe.Graphics/Image/DDSLoader.cs
@@ -130,5 +130,29 @@ namespace Zeckoxe.Image
             Cubemap = 0x00000008, 
         }
 
+
+
+        [Flags]
+        public enum CubemapFlags
+        {
+            CubeMap = 0x00000200, 
+
+            Volume = 0x00200000, 
+
+            PositiveX = 0x00000600, 
+
+            NegativeX = 0x00000a00, 
+
+            PositiveY = 0x00001200, 
+
+            NegativeY = 0x00002200, 
+
+            PositiveZ = 0x00004200, 
+
+            NegativeZ = 0x00008200, 
+
+            AllFaces = PositiveX | NegativeX | PositiveY | NegativeY | PositiveZ | NegativeZ,
+        }
+
     }
 }
