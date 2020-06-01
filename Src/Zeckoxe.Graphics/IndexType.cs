272159e989df1243e6f0ce7c8f3ba71b7069ccf8
Faber Sanchez <faber.reach@gmail.com>
2020-06-01 06:56:45 -0500

    Update IndexType

--- Src/Zeckoxe.Graphics/IndexType.cs
+++ Src/Zeckoxe.Graphics/IndexType.cs
@@ -1,20 +1,23 @@
 ﻿// Copyright (c) 2019-2020 Faber Leonardo. All Rights Reserved.
 
 /*=============================================================================
-	VkIndexType.cs
+	IndexType.cs
 =============================================================================*/
 
 using System;
-
 
 namespace Zeckoxe.Graphics
 {
     public enum IndexType
     {
         Uint16 = 0,
+
         Uint32 = 1,
+
         NoneKHR = 1000165000,
+
         NoneNV = 1000165000,
+
         Uint8EXT = 1000265000
     }
 }
