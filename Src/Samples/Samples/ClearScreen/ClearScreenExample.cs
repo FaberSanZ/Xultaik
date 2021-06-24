9dd21cec7f32801c5182306645cf595bb542f4a6
Faber Sanchez <faber.reach@gmail.com>
2021-06-23 20:32:03 -0500

    Update ClearScreenExample

--- Src/Samples/Samples/ClearScreen/ClearScreenExample.cs
+++ Src/Samples/Samples/ClearScreen/ClearScreenExample.cs
@@ -4,15 +4,17 @@ using System.Diagnostics;
 using System.Text;
 using Vultaik.Desktop;
 using Vultaik;
+using Samples.Common;
+using Vultaik.Toolkit;
 
 namespace Samples.ClearScreen
 {
-    public class ClearScreenExample : IDisposable
+    public class ClearScreenExample : ExampleBase, IDisposable
     {
-        public ClearScreenExample()
-        {
-            Window = new("Vultaik - (ClearScreen)", 1200, 800);
+        private float r, g, b, mod_r, mod_g, mod_b;
 
+        public ClearScreenExample() : base()
+        {
             Parameters = new()
             {
                 BackBufferWidth = Window.Width,
@@ -27,141 +29,87 @@ namespace Samples.ClearScreen
         }
 
 
-        public Window Window { get; set; }
-        public PresentationParameters Parameters { get; set; }
-        public Adapter Adapter { get; set; }
-        public Device Device { get; set; }
-        public Framebuffer Framebuffer { get; set; }
-        public SwapChain SwapChain { get; set; }
-
-        public CommandBuffer CommandBuffer { get; set; }
-
-
-        public float R { get; set; }
-        public float G { get; set; }
-        public float B { get; set; }
-        public float A { get; set; }
-
-        private float ModR = 20;
-        private float ModG = 20;
-        private float ModB = 20;
+        public PresentationParameters Parameters;
+        public Adapter Adapter;
+        public Device Device;
+        public Framebuffer Framebuffer;
+        public SwapChain SwapChain;
+        public CommandBuffer CommandBuffer;
 
 
 
-        public void Initialize()
+
+        public override void Initialize()
         {
 
             Adapter = new(Parameters);
-
             Device = new(Adapter);
-
+            CommandBuffer = new(Device, CommandBufferType.AsyncGraphics);
 
             SwapChain = new(Device, new()
             {
-                Source = GetSwapchainSource(),
+                Source = GetSwapchainSource(Adapter),
                 ColorSrgb = false,
                 Height = Window.Height,
                 Width = Window.Width,
                 SyncToVerticalBlank = false,
                 DepthFormat = Adapter.DepthFormat is Vortice.Vulkan.VkFormat.Undefined ? null : Adapter.DepthFormat
             });
-
             Framebuffer = new(SwapChain);
-
-            CommandBuffer = new(Device, CommandBufferType.AsyncGraphics);
-        }
-
-        public SwapchainSource GetSwapchainSource()
-        {
-            if (Adapter.SupportsSurface)
-            {
-                if (Adapter.SupportsWin32Surface)
-                    return Window.SwapchainWin32;
-
-                if (Adapter.SupportsX11Surface)
-                    return Window.SwapchainX11;
-
-                if (Adapter.SupportsWaylandSurface)
-                    return Window.SwapchainWayland;
-
-                if (Adapter.SupportsMacOSSurface)
-                    return Window.SwapchainNS;
-            }
-
-            throw new PlatformNotSupportedException("Cannot create a SwapchainSource.");
         }
 
 
-        private void Window_Resize((int Width, int Height) obj)
+        public override void Resize(int width, int height)
         {
-            Console.WriteLine($"Height: {obj.Height}");
-            Console.WriteLine($"Width: {obj.Width}");
+            Console.WriteLine($"Height: {height}");
+            Console.WriteLine($"Width: {width}");
             Console.WriteLine("=======");
 
             Device.WaitIdle();
-            SwapChain.Resize(obj.Width, obj.Height);
+            SwapChain.Resize(width, height);
             Framebuffer.Resize();
         }
 
 
-        public void Run()
-        {
-            Initialize();
 
-            BeginRun();
-
-            Window?.Show();
-            Window!.Resize += Window_Resize;
-            Tick();
-        }
-
-        public void Tick()
-        {
-
-            Window.RenderLoop(() =>
-            {
-                Update();
-                Draw();
-            });
-        }
-
-
-
-        public void BeginRun()
-        {
-
-        }
-
-        public void Update()
-        {
-            //Update the colors of our scene
-            R += ModR * 0.00005f;
-            G += ModG * 0.00002f;
-            B += ModB * 0.00001f;
-
-            if (R >= 1.0f || R <= 0.0f)
-                ModR *= -1;
-
-            if (G >= 1.0f || G <= 0.0f)
-                ModG *= -1;
-
-            if (B >= 1.0f || B <= 0.0f)
-                ModB *= -1;
-        }
-
-        public void Draw()
+        public override void Draw(ApplicationTime time)
         {
 
             Device.WaitIdle();
 
             CommandBuffer.Begin();
-            CommandBuffer.BeginFramebuffer(Framebuffer, R, G, B, A);
+            CommandBuffer.BeginFramebuffer(Framebuffer, r, g, b, 1);
 
             CommandBuffer.Close();
             Device.Submit(CommandBuffer);
 
             SwapChain.Present();
         }
+
+
+
+
+
+
+
+        public override void Update(ApplicationTime time)
+        {
+            if (mod_r == 0) mod_r = mod_g = mod_b += 20;
+            //Update the colors of our scene
+            r += mod_r * 0.00005f;
+            g += mod_g * 0.00002f;
+            b += mod_b * 0.00001f;
+
+            if (r >= 1.0f || r <= 0.0f)
+                mod_r *= -1;
+
+            if (g >= 1.0f || g <= 0.0f)
+                mod_g *= -1;
+
+            if (b >= 1.0f || b <= 0.0f)
+                mod_b *= -1;
+        }
+
 
         public void Dispose()
         {
