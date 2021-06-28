967c63ae0c824e167e273ac02f01e8f1d6f3d662
Faber Sanchez <faber.reach@gmail.com>
2021-06-27 23:11:23 -0500

    Add StringBuilder for ConsoleLog

--- Src/Vultaik/Core/ConsoleLog.cs
+++ Src/Vultaik/Core/ConsoleLog.cs
@@ -3,15 +3,24 @@
 
 
 using System;
+using System.Text;
 
 namespace Vultaik
 {
     public static class ConsoleLog
     {
+        static ConsoleLog()
+        {
+            Data = new();
+        }
+        public static StringBuilder Data { get; set; } 
+
         public static void Info(string type, string message)
         {
             WriteColored(ConsoleColor.Green, $"[INFO] [{type}]");
             Console.WriteLine(" " + message + "\n");
+
+            Data.Append($"[INFO] [{type}] " + message + "\n");
         }
 
         public static void Warn(string type, string message)
