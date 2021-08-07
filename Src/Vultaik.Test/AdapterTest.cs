32038b88ed5cddb3727ce9f3c580139c8464f8ee
Faber Sanchez <faber.reach@gmail.com>
2021-08-06 20:01:20 -0500

    Add test for SwapChain support

--- Src/Vultaik.Test/AdapterTest.cs
+++ Src/Vultaik.Test/AdapterTest.cs
@@ -10,23 +10,33 @@ namespace Vultaik.Test
         {
             AdapterConfig config = new();
             using Adapter adapter = new(config);
+            bool supported = adapter.IsSupported;
 
-            bool IsSupported = adapter.IsSupported;
-
-            Assert.True(IsSupported, "Vulkan is not supported");
+            Assert.True(supported, "Vulkan is not supported.");
         }
 
         [Fact]
-        public void Dispose_Adapter()
+        public void DisposeAdapter()
         {
             AdapterConfig config = new();
             Adapter adapter = new(config);
 
-            Assert.False(adapter.IsNull, "Adapter is not disposed");
+            Assert.False(adapter.IsNull, "Adapter is not disposed.");
 
             adapter.Dispose();
-            Assert.True(adapter.IsNull, "Adapter is disposed");
+            Assert.True(adapter.IsNull, "Adapter is disposed.");
 
+        }
+
+
+        [Fact]
+        public void SwapChainSupport()
+        {
+            AdapterConfig config = new();
+            using Adapter adapter = new(config);
+            bool support = adapter.SwapChain.Support;
+
+            Assert.True(support, "SwapChain is not supported.");
         }
     }
 }
